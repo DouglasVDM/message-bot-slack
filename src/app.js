3ea0d4a92fe4f56f28f661b0a59a5c25daa4ea51
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-25 19:46:59 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import { RTMClient } from "@slack/rtm-api";
+import { BOT_SPAM_CHANNEL, SLACK_OAUTH_TOKEN } from "./constants";
+import { WebClient } from "@slack/web-api";
+const packageJson = require("../package.json")
+
+const rtm = new RTMClient(SLACK_OAUTH_TOKEN);
+const web = new WebClient(SLACK_OAUTH_TOKEN);
+
+rtm.start()
+  .catch(console.error);
+
+rtm.on("ready", async () => {
+  console.log("bot started");
+  sendMessage(BOT_SPAM_CHANNEL, `Bot version ${packageJson.version} is online`)
+});
+
+rtm.on('slack_event', async (eventType, event) => {
+  if (event && event.type === 'message') {
+    if (event.text === '!hello') {
+      hello(event.channel, event.user)
+    }
+  }
+})
+
+function hello(channelId, userId) {
+  sendMessage(channelId, `Heya! <@${userId}>`)
+}
+
+async function sendMessage(channel, message) {
+  await web.chat.postMessage({
+    channel: channel,
+    text: message,
+  })
+}
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const SLACK_OAUTH_TOKEN = "xoxb-2204486591542-2208679574293-lMebmY1ArBOHEHOsynh3ZbvI";
+export const BOT_SPAM_CHANNEL = "#bot-spam";
